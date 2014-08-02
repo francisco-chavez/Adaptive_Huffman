a93c2954de6bf4649cc40422cf9ba0a5bc7672af
francisco-chavez <francisco.chavez.509@outlook.com>
2014-08-01 23:19:16 -0700

    Working on the Implementation of the HuffmanTree class.
    The HuffmanTree will be the backbone of both the encoder and the
    decoder.

--- SourceCode/AdaptiveHuffmanLib/HuffmanTree.cs
+++ SourceCode/AdaptiveHuffmanLib/HuffmanTree.cs
@@ -16,6 +16,17 @@ namespace Unv.AdaptiveHuffmanLib
 		#endregion
 
 
+		#region Properties
+		private TreeNode EmptyNode
+		{
+			get
+			{
+				return _head.Next;
+			}
+		}
+		#endregion
+
+
 		#region Constructors
 		public HuffmanTree()
 		{
@@ -33,18 +44,35 @@ namespace Unv.AdaptiveHuffmanLib
 
 
 		#region Methods
-		private bool ContainsCharacter(char character)
+		public void InsertCharacter(char character)
 		{
+			bool alreadyPresent = ContainsCharacter(character);
+
+			if (!alreadyPresent)
+				AddCharacter(character);
+			else
+				IncreamentCharacter(character);
+
+			_root.UpdateFrequency();
+
+			BalanceTree();
+		}
+
+		public bool ContainsCharacter(char character)
+		{
+			// The most frequent character is going to be  towards
+			// the end of the linked list. So, if we start at the
+			// back, and moves towards the front, we will find is
+			// sooner (on average).
 			bool		result		= false;
-			TreeNode	currentNode = _head.Next;
-			
-			while (currentNode != _tail)
+			TreeNode	currentNode = _tail.Prev;
+
+			while (currentNode != _head)
 			{
-				if (currentNode.IsEmpty)
+				if (currentNode.IsEmpty || !currentNode.IsLeaf)
 				{
-					// The empty node doesn't affect the result of this
-					// search, so we're not doing anything specific with
-					// the empty node.
+					// If the current node isn't an empty leaf node,
+					// then we don't want it to be marked down.
 				}
 				else if (currentNode.Character == character)
 				{
@@ -52,39 +80,135 @@ namespace Unv.AdaptiveHuffmanLib
 					break;
 				}
 
-				currentNode = currentNode.Next;
+				currentNode = currentNode.Prev;
 			}
 
 			return result;
 		}
+
+		private void AddCharacter(char character)
+		{
+			// The empty node will become a branch node, so the
+			// temp reference we're using is called newBranch.
+			// Besides, the left child of the newBranch will become
+			// the empty node.
+			TreeNode newBranch = EmptyNode;
+			newBranch.Left	= new TreeNode();
+			newBranch.Right = new TreeNode();
+
+			newBranch.Left.Root			= newBranch;
+			newBranch.Right.Right		= newBranch;
+
+			newBranch.Right.Character	= character;
+			newBranch.Right.Frequency	= 1;
+
+			newBranch.Left.Next			= newBranch.Right;
+			newBranch.Left.Prev			= newBranch.Prev;
+
+			newBranch.Right.Next		= newBranch;
+			newBranch.Right.Prev		= newBranch.Left;
+
+			newBranch.Prev	= newBranch.Right;
+
+			TreeNode parent = newBranch;
+			while (parent != null)
+			{
+				parent.Frequency = parent.Left.Frequency + parent.Right.Frequency;
+				parent = parent.Root;
+			}
+		}
+
+		private void IncreamentCharacter(char character)
+		{
+			TreeNode currentNode = _tail.Prev;
+
+			while (true)
+			{
+				if (currentNode.IsLeaf && currentNode.Character == character)
+					break;
+
+				currentNode = currentNode.Prev;
+			}
+
+			currentNode.Frequency++;
+		}
+
+		private void SwitchChildNodes(TreeNode parent)
+		{
+			TreeNode start	= parent.Left.Prev;
+			TreeNode end	= parent.Right.Next;
+			TreeNode temp	= parent.Left;
+
+			// Switch childs nodes in tree
+			parent.Left = parent.Right;
+			parent.Right = temp;
+
+			//
+			// Re-arrange nodes in Linked list
+			//
+
+			// Connect starting node to left node
+			start.Next = parent.Left;
+			parent.Left.Prev = start;
+
+			// Connect left node to right node
+			parent.Left.Next = parent.Right;
+			parent.Right.Prev = parent.Left;
+
+			// Connect right node to ending node
+			parent.Right.Next = end;
+			end.Prev = parent.Right;
+		}
+
+		private void BalanceTree()
+		{
+			throw new NotImplementedException();
+		}
 		#endregion
+
 
 		private class TreeNode
 		{
-			#region Attributes
-			private char _character;
-			private bool _isEmpty	= true;
-			#endregion
-
-
 			#region Properties
 			public TreeNode Next		{ get; set; }
 			public TreeNode Prev		{ get; set; }
-											   	    
+
+			public TreeNode Root		{ get; set; }
 			public TreeNode Left		{ get; set; }
 			public TreeNode Right		{ get; set; }
-			public TreeNode Root		{ get; set; }
+
+			public bool		IsLeaf		{ get { return Right == null && Left == null; } }
 
 			public int		Frequency	{ get; set; }
-			public bool		IsEmpty		{ get { return _isEmpty; } }
+			public bool		IsEmpty		{ get { return IsLeaf && Frequency == 0; } }
 
-			public char Character
+			public char		Character	{ get; set; }
+			#endregion
+
+
+			#region Constructors
+			public TreeNode()
 			{
-				get { return _character; }
-				set
+				this.Frequency	= 0;
+				this.Left		= null;
+				this.Next		= null;
+				this.Prev		= null;
+
+				this.Right		= null;
+				this.Root		= null;
+			}
+			#endregion
+
+
+			#region Methods
+			public void UpdateFrequency()
+			{
+				if (!IsLeaf)
 				{
-					_character	= value;
-					_isEmpty	= false;
+					Left.UpdateFrequency();
+					Right.UpdateFrequency();
+
+					Frequency = Left.Frequency + Right.Frequency;
 				}
 			}
 			#endregion
