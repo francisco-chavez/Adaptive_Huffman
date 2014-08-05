ed8cbdb7041366baf0f89df508e48559bd1d1f9c
francisco-chavez <francisco.chavez.509@outlook.com>
2014-08-04 21:19:22 -0700

    Added a Test for the Huffman Tree's encoding

--- SourceCode/TestDemo01/Program.cs
+++ SourceCode/TestDemo01/Program.cs
@@ -5,12 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Unv.AdaptiveHuffmanLib;
+
+
 namespace TestDemo01
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			HuffmanTree tree = new HuffmanTree();
+			string testInput = "mississippi river";
+			BitArray encodedCharacter;
+			string[] treeInfo;
+
+			treeInfo = tree.GetTestReadout();
+
+			for (int i = 0; i < testInput.Length; i++)
+			{
+				encodedCharacter = tree.InsertCharacter(testInput[i]);
+				treeInfo = tree.GetTestReadout();
+			}
 		}
 	}
 }
